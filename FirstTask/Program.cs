84c027083328ab7a217c7af5d2d698ac63b3d14e
nikita <zherko.na@phystech.edu>
2022-11-18 14:38:21 +0300

    add Exercise 2

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// получить пользовательский ввод
+Console.Write("Введите целое число: ");
+string UserInput = Console.ReadLine() ?? "";
+
+// преобразовать строку в числ
+int number = int.Parse(UserInput);
+
+// вычислячем квадрат числа
+int square = number * number;
+
+// вывод резулитата
+Console.WriteLine($"{number} -> {square}");
+
+
---
+++ ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿// программа принимает на вход два числа и выдает максимальное из них
+
+Console.WriteLine("Данная программа определяеь максимальное из двух чисел");
+
+Console.Write("Введите первое целое число: ");
+string FirstNum = Console.ReadLine();
+Console.Write("Введите второе целое число: ");
+string SecondNum = Console.ReadLine();
+
+int A = int.Parse(FirstNum);
+int B = int.Parse(SecondNum);
+
+if(A >= B)
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine($"A = {A}");
+}
+else
+{
+    Console.Write("Максимальное число: ");
+    Console.WriteLine($"B = {B}");
+}
