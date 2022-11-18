c9dd28e1cde9fb89d0cb1b5e77ed80477cacf990
nikita <zherko.na@phystech.edu>
2022-11-18 14:40:33 +0300

    Add Exercise 6

---
+++ ex6/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимаем число и определяем является ли оно четным/нечетным
+Console.WriteLine("Опредлеим является число четным или нечетным. ");
+Console.Write("Введите число: ");
+string UserNumber = Console.ReadLine();
+
+int number = int.Parse(UserNumber);
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число явяляется четным.");
+}
+else
+{
+    Console.WriteLine("Число явяляется нечетным.");
+}
