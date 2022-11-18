d553ecf00e0e55c7e47ea97bc5b13b05299747d0
nikita <zherko.na@phystech.edu>
2022-11-18 14:40:55 +0300

    Ad exercise 8

---
+++ ex8/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа на вход принимает число N и выводит все четные числа от 1 до N
+
+Console.WriteLine("Вывод всех четных чисел от 1 до N");
+Console.Write("Введите число N: ");
+string UserN = Console.ReadLine();
+
+int N = int.Parse(UserN);
+
+int i = 0;
+while(i <= N)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+    i ++;
+}
